b8e7a6f61d9af97f00d01f65cd604008ded1932c
Sean Murphy <sean@iamseanmurphy.com>
2025-07-21 04:32:19 +0000

    1.0.0.93

--- newsair/inc/ansar/customizer-admin/newsair-admin-plugin-install.php
+++ newsair/inc/ansar/customizer-admin/newsair-admin-plugin-install.php
@@ -1,4 +1,5 @@
-<?php /**
+<?php 
+/**
  * AJAX handler to store the state of dismissible notices.
  */
 function newsair_ajax_notice_handler() {
@@ -13,68 +14,80 @@ function newsair_ajax_notice_handler() {
 add_action( 'wp_ajax_newsair_dismissed_notice_handler', 'newsair_ajax_notice_handler' );
 
 function newsair_deprecated_hook_admin_notice() {
-        // Check if it's been dismissed...
-        if ( ! get_option('dismissed-get_started', FALSE ) ) {
-            // Added the class "notice-get-started-class" so jQuery pick it up and pass via AJAX,
-            // and added "data-notice" attribute in order to track multiple / different notices
-            // multiple dismissible notice states ?>
-               <div class="newsair-notice-started updated notice notice-get-started-class is-dismissible" data-notice="get_started">
-                <div class="newsair-notice clearfix">
-                    <div class="newsair-notice-content">
-                        
-                        <div class="newsair-notice_text">
-                        <div class="newsair-hello">
-                            <?php esc_html_e( 'Hello, ', 'newsair' ); 
-                            $current_user = wp_get_current_user();
-                            echo esc_html( $current_user->display_name );
-                            ?>
-                            <img draggable="false" role="img" class="emoji" alt="👋🏻" src="https://s.w.org/images/core/emoji/14.0.0/svg/1f44b-1f3fb.svg">                
-                        </div>
-                        <h1><?php
-                                $theme_info = wp_get_theme();
-                                printf( esc_html__('Welcome to %1$s', 'newsair'), esc_html( $theme_info->Name ), esc_html( $theme_info->Version ) ); ?>
-                        </h1>
-                        
-                        <p><?php esc_html_e("Thank you for choosing Newsair theme. To take full advantage of the complete features of the theme click the Starter Sites and Install and Activate the", "newsair");?> <a href="https://wordpress.org/plugins/ansar-import"><?php esc_html_e("Ansar Import", "newsair");?></a> <?php esc_html_e("plugin then use the demo importer and install the Newsair Demo according to your need.", "newsair"); ?></p>
+    // Check if it's been dismissed...
+    if ( ! get_option('dismissed-get_started', FALSE ) ) {
+        // Added the class "notice-get-started-class" so jQuery pick it up and pass via AJAX,
+        // and added "data-notice" attribute in order to track multiple / different notices
+        // multiple dismissible notice states ?>
+            <div class="newsair-notice-started updated notice notice-get-started-class is-dismissible" data-notice="get_started">
+            <div class="newsair-notice clearfix">
+                <div class="newsair-notice-content">
+                    
+                    <div class="newsair-notice_text">
+                    <div class="newsair-hello">
+                        <?php esc_html_e( 'Hello, ', 'newsair' ); 
+                        $current_user = wp_get_current_user();
+                        echo esc_html( $current_user->display_name );
+                        ?>
+                        <img draggable="false" role="img" class="emoji" alt="👋🏻" src="https://s.w.org/images/core/emoji/14.0.0/svg/1f44b-1f3fb.svg">                
+                    </div>
+                    <h1><?php
+                            $theme_info = wp_get_theme();
+                            printf( esc_html__('Welcome to %1$s', 'newsair'), esc_html( $theme_info->Name ), esc_html( $theme_info->Version ) ); ?>
+                    </h1>
+                    
+                    <p>
+                    <?php
+                        echo wp_kses_post( sprintf(
+                            __(
+                                'Thank you for choosing %1$s theme. To take full advantage of the complete features of the theme, click Get Started and install and activate the %2$s plugin, then use the demo importer and install the %3$s demo according to your need.',
+                                'newsair'
+                            ),
+                            esc_html($theme_info->Name),
+                            '<a href="https://wordpress.org/plugins/ansar-import" target="_blank">' . esc_html__('Ansar Import', 'newsair') . '</a>',
+                            esc_html($theme_info->Name)
+                        ) );
+                        ?>
+                    </p>
 
-                        <div class="panel-column-6">
-                            <div class="newsair-notice-buttons">
-                                <a class="newsair-btn-get-started button button-primary button-hero newsair-button-padding" href="#" data-name="" data-slug=""><span aria-hidden="true" class="dashicons dashicons-images-alt"></span><?php esc_html_e( 'Get Started', 'newsair' ) ?></a>
-                                <a class="newsair-btn-get-started-customize button button-secondary button-hero newsair-button-padding" href="<?php echo esc_url( admin_url( '/customize.php' ) ); ?>" data-name="" data-slug=""><span aria-hidden="true" class="dashicons dashicons-welcome-widgets-menus"></span><?php esc_html_e( 'Customize Site', 'newsair' ) ?></a>
+                    <div class="panel-column-6">
+                        <div class="newsair-notice-buttons">
+                            <a class="newsair-btn-get-started button button-primary button-hero newsair-button-padding" href="#" data-name="" data-slug=""><span aria-hidden="true" class="dashicons dashicons-images-alt"></span><?php esc_html_e( 'Get Started', 'newsair' ) ?></a>
+                            <a class="newsair-btn-get-started-customize button button-secondary button-hero newsair-button-padding" href="<?php echo esc_url( admin_url( '/customize.php' ) ); ?>" data-name="" data-slug=""><span aria-hidden="true" class="dashicons dashicons-welcome-widgets-menus"></span><?php esc_html_e( 'Customize Site', 'newsair' ) ?></a>
+                        </div>
+                        <div class="newsair-notice-links">
+                            <div class="newsair-demos newsair-notice-link">
+                                <span aria-hidden="true" class="dashicons dashicons-images-alt"></span>
+                                <a class="newsair-demos" href="<?php echo esc_url('https://demos.themeansar.com/newsair-demos')?>" data-name="" data-slug=""><?php esc_html_e( 'View Demos', 'newsair' ) ?></a>
                             </div>
-                            <div class="newsair-notice-links">
-                                <div class="newsair-demos newsair-notice-link">
-                                    <span aria-hidden="true" class="dashicons dashicons-images-alt"></span>
-                                    <a class="newsair-demos" href="<?php echo esc_url('https://demos.themeansar.com/newsair-demos')?>" data-name="" data-slug=""><?php esc_html_e( 'View Demos', 'newsair' ) ?></a>
-                                </div>
-                                <div class="newsair-documentation newsair-notice-link">
-                                    <span aria-hidden="true" class="dashicons dashicons-list-view"></span>
-                                    <a class="newsair-documentation" href="<?php echo esc_url('https://docs.themeansar.com/docs/newsair-lite/')?>" data-name="" data-slug=""><?php esc_html_e( 'View Documentation', 'newsair' ) ?></a>
-                                </div>
-                                <div class="newsair-support newsair-notice-link">
-                                    <span aria-hidden="true" class="dashicons dashicons-format-chat"></span>
-                                    <a class="newsair-support" href="<?php echo esc_url('https://themeansar.ticksy.com/')?>" data-name="" data-slug=""><?php esc_html_e( 'Support', 'newsair' ) ?></a>
-                                </div>
-                                <div class="newsair-videos newsair-notice-link">
-                                    <span aria-hidden="true" class="dashicons dashicons-video-alt3"></span>
-                                    <a class="newsair-videos" href="<?php echo esc_url('https://www.youtube.com/watch?v=EhmHmAXPrqg&list=PLWpTqYqS4j-xSJIiuDcYWIHXO8vDl4M-4')?>" data-name="" data-slug=""><?php esc_html_e( 'Video Tutorials', 'newsair' ) ?></a>
-                                </div>
+                            <div class="newsair-documentation newsair-notice-link">
+                                <span aria-hidden="true" class="dashicons dashicons-list-view"></span>
+                                <a class="newsair-documentation" href="<?php echo esc_url('https://docs.themeansar.com/docs/newsair-lite/')?>" data-name="" data-slug=""><?php esc_html_e( 'View Documentation', 'newsair' ) ?></a>
+                            </div>
+                            <div class="newsair-support newsair-notice-link">
+                                <span aria-hidden="true" class="dashicons dashicons-format-chat"></span>
+                                <a class="newsair-support" href="<?php echo esc_url('https://themeansar.ticksy.com/')?>" data-name="" data-slug=""><?php esc_html_e( 'Support', 'newsair' ) ?></a>
+                            </div>
+                            <div class="newsair-videos newsair-notice-link">
+                                <span aria-hidden="true" class="dashicons dashicons-video-alt3"></span>
+                                <a class="newsair-videos" href="<?php echo esc_url('https://www.youtube.com/watch?v=EhmHmAXPrqg&list=PLWpTqYqS4j-xSJIiuDcYWIHXO8vDl4M-4')?>" data-name="" data-slug=""><?php esc_html_e( 'Video Tutorials', 'newsair' ) ?></a>
                             </div>
                         </div>
+                    </div>
 
-                        </div>
-                        <div class="newsair-notice_image">
-                        <?php 
-                        $image_url = get_theme_file_uri( '/images/newsair.customize.webp' );
-                        // Check if the file exists
-                        if ( file_exists( get_theme_file_path( '/images/newsair.customize.webp' ) ) ) { ?>
-                            <img class="newsair-screenshot" src="<?php echo esc_url( $image_url ); ?>" alt="<?php esc_attr_e( 'Newsair', 'newsair' ); ?>" />
-                        <?php } ?>
-                        </div>
+                    </div>
+                    <div class="newsair-notice_image">
+                    <?php 
+                    $image_url = get_theme_file_uri( '/images/newsair.customize.webp' );
+                    // Check if the file exists
+                    if ( file_exists( get_theme_file_path( '/images/newsair.customize.webp' ) ) ) { ?>
+                        <img class="newsair-screenshot" src="<?php echo esc_url( $image_url ); ?>" alt="<?php esc_attr_e( 'Newsair', 'newsair' ); ?>" />
+                    <?php } ?>
                     </div>
                 </div>
             </div>
-        <?php }
+        </div>
+    <?php }
 }
 
 add_action( 'admin_notices', 'newsair_deprecated_hook_admin_notice' );
--- newsair/readme.txt
+++ newsair/readme.txt
@@ -4,8 +4,8 @@ Author: Themeansar
 Requires at least: WordPress 6.7
 Tested up to: WordPress 6.8
 Requires PHP: 7.4
-Stable tag: 1.0.0.92
-Version: 1.0.0.92
+Stable tag: 1.0.0.93
+Version: 1.0.0.93
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Tags: one-column, two-columns ,right-sidebar, flexible-header, custom-background, custom-header, custom-menu, editor-style, featured-images, footer-widgets,  theme-options, threaded-comments, rtl-language-support, translation-ready, full-width-template, custom-logo, blog, news
@@ -403,3 +403,6 @@ Source: https://stocksnap.io/photo/train-station-6FHSE43K4R
 
 = Version 1.0.0.92
 * Fixed Notice.
+
+= Version 1.0.0.93
+* Update Notice.
