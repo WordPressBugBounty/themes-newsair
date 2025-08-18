bdd526d40e7143615f5017473ee80e7f90fa51c6
Sean Murphy <sean@iamseanmurphy.com>
2025-08-18 03:46:02 +0000

    1.0.0.96

--- newsair/inc/ansar/font/font.php
+++ newsair/inc/ansar/font/font.php
@@ -7,42 +7,53 @@ add_action( 'enqueue_block_editor_assets', 'newsair_theme_fonts',1 );
 add_action( 'customize_preview_init', 'newsair_theme_fonts', 1 );
 
 function newsair_theme_fonts() {
-        $fonts_url = newsair_fonts_url();
-        // Load Fonts if necessary.
-        if ( $fonts_url ) {
-            require_once get_theme_file_path( 'inc/ansar/font/wptt-webfont-loader.php' );
-            wp_enqueue_style( 'newsair-theme-fonts', wptt_get_webfont_url( $fonts_url ), array(), '20201110' );
-        }
+    $fonts_url = newsair_fonts_url();
+    // Load Fonts if necessary.
+    if ( $fonts_url ) {
+        require_once get_theme_file_path( 'inc/ansar/font/wptt-webfont-loader.php' );
+        wp_enqueue_style( 'newsair-theme-fonts', wptt_get_webfont_url( $fonts_url ), array(), '20201110' );
+    }
+}
+
+function newsair_get_google_fonts_list() {
+    return array(
+        'DM Sans',
+        'Open Sans',
+        'Kalam',
+        'Rokkitt',
+        'Jost',
+        'Poppins',
+        'Lato',
+        'Noto Serif',
+        'Raleway',
+        'Roboto',
+        'Inter Tight',
+    );
 }
 
 function newsair_fonts_url() {
-	
     $fonts_url = '';
-		
+    
+    // Step 1: Get font names
+    $fonts = newsair_get_google_fonts_list();
+    
+    // Step 2: Set a common weight string
+    $common_weights = '100,200,300,400,500,600,700,800,900';
+    
+    // Step 3: Format fonts for Google Fonts URL
     $font_families = array();
-        
-        $font_families = array(
-            'DM Sans:300,400,500,700',
-            'Open Sans:300,400,600,700',
-            'Kalam:300,400,700',
-            'Rokkitt:300,400,700',
-            'Jost:300,400,500,700',
-            'Poppins:300,400,500,600,700',
-            'Lato:300,400,700',
-            'Noto Serif:300,400,700',
-            'Raleway:300,400,500,700',
-            'Roboto:300,400,500,700',
-            'Inter Tight:100,200,300,400,500,600,700,800,900',
-        );
+    foreach ( $fonts as $font_name ) {
+        $font_families[] = $font_name . ':' . $common_weights;
+    }
 
-        // Build the URL
-        $query_args = array(
-            'family' => urlencode( implode( '|', $font_families ) ),
-            'display' => 'swap',
-            'subset' => 'latin,latin-ext',
-        );
- 
-    return apply_filters( 'newsair_fonts_url', add_query_arg( $query_args, 'https://fonts.googleapis.com/css' ) );
+    // Step 4: Generate the full Google Fonts URL
+    $query_args = array(
+        'family' => urlencode( implode( '|', $font_families ) ),
+        'display' => 'swap',
+        'subset'  => 'latin,latin-ext',
+    );
+
+    $fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
 
     return $fonts_url;
 }
--- newsair/readme.txt
+++ newsair/readme.txt
@@ -4,8 +4,8 @@ Author: Themeansar
 Requires at least: WordPress 6.7
 Tested up to: WordPress 6.8
 Requires PHP: 7.4
-Stable tag: 1.0.0.95
-Version: 1.0.0.95
+Stable tag: 1.0.0.96
+Version: 1.0.0.96
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Tags: one-column, two-columns ,right-sidebar, flexible-header, custom-background, custom-header, custom-menu, editor-style, featured-images, footer-widgets,  theme-options, threaded-comments, rtl-language-support, translation-ready, full-width-template, custom-logo, blog, news
@@ -412,3 +412,6 @@ Source: https://stocksnap.io/photo/train-station-6FHSE43K4R
 
 = Version 1.0.0.95
 * Fixed Footer List Styling Issues.
+
+= Version 1.0.0.96
+* Fixed Typography Fonts.
